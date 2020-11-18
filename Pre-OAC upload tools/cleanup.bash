e1810d93e51271e37844fac16cbe4c78318facb6
Liz Phillips <lizcphillips@users.noreply.github.com>
2020-11-17 17:11:00 -0800

    Update cleanup.bash
    Changed two sets of sed delimiters

--- Pre-OAC upload tools/cleanup.bash
+++ Pre-OAC upload tools/cleanup.bash
@@ -5,7 +5,7 @@ for file in $path/*.xml
 do
     tidy -emq -config /usr/share/tidy1.cfg $file
     cp $file $file.bak
-    cat $file | sed 's/Davis General Library/Davis Library/g' | sed 's/"creator"/"Creator"/' | sed 's/latn/Latn/g' | sed 's|http://www.lib.ucdavis.edu/dept/specol/|https://www.library.ucdavis.edu/archives-and-special-collections|g' | sed 's|"0/0"|"0000"|g' | sed 's/xlink:audience="internal"//g' | sed 's/xlink:audience="external"//g' | sed 's/ns2:type="simple"//g' | sed 's|<dsc />||g' | sed 's|,</unittitle>|</unittitle>|g' > $file.2
+    cat $file | sed 's/Davis General Library/Davis Library/g' | sed 's/"creator"/"Creator"/' | sed 's/latn/Latn/g' | sed 's|http://www.lib.ucdavis.edu/dept/specol/|https://www.library.ucdavis.edu/archives-and-special-collections|g' | sed 's|"0/0"|"0000"|g' | sed 's|xlink:audience="internal"||g' | sed 's|xlink:audience="external"||g' | sed 's/ns2:type="simple"//g' | sed 's|<dsc />||g' | sed 's|,</unittitle>|</unittitle>|g' > $file.2
     mv $file.2 $file
 done
 diff -b  $path/*.bak $path/*.xml | less
