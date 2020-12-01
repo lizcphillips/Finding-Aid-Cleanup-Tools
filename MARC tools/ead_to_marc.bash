a3193fa339ba41321a3c17038a6a1a37084fae0a
Liz Phillips <lizcphillips@users.noreply.github.com>
2020-12-01 12:25:45 -0800

    Update ead_to_marc.bash

--- MARC tools/ead_to_marc.bash
+++ MARC tools/ead_to_marc.bash
@@ -5,7 +5,7 @@ for file in $path/*.xml
 do
     tidy -emq -config /usr/share/tidy1.cfg $file
     cp $file $file.bak
-    cat $file | sed 's|xmlns="urn:isbn:1-931666-22-9" ||g' | sed 's/xlink://g' | sed 's/linear feet//g' | sed 's|<head>Preferred Citation</head>||g' | sed 's|<head>History</head>||g' | sed 's|<head>Biography</head>||g'  | sed 's|.</p>|. </p>|g' > $file.2
+    cat $file | sed 's|xmlns="urn:isbn:1-931666-22-9" ||g' | sed 's/xlink://g' | sed 's/linear feet//g' | sed 's|<head>Preferred Citation</head>||g' | sed 's|<head>History</head>||g' | sed 's|<head>Scope and Contents</head>||g' | sed 's|<head>Biography</head>||g'  | sed 's|.</p>|. </p>|g' > $file.2
     mv $file.2 $file
 done
 diff -b  $path/*.bak $path/*.xml | less
