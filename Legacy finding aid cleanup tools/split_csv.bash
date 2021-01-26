35b8b231433a5bc4639f0f5e97db06bf3acf47e4
Liz Phillips <lizcphillips@users.noreply.github.com>
2021-01-26 11:55:49 -0800

    Treat first split differently

--- Legacy finding aid cleanup tools/split_csv.bash
+++ Legacy finding aid cleanup tools/split_csv.bash
@@ -8,15 +8,22 @@ do
     #define tail
     data=$(tail -n +2 $file)
     #split up file
-    echo $data | split -l 2000 $file $file.2
+    echo $data | split -l 1000 $file $file.2
     #$file.2* (* part) is a glob - this will go through all of the files
     #tempfile gives the for loop something to grab & work with
+    #this counter makes it possible to treat the first split differently
+    (( i=0 ))
     for tempfile in $file.2*
     do
-	#pass header back to parts
-	#sed statement within quotes: 1i is its own command (see man page)
-	sed -i "1i$header" $tempfile
-	#rename file to .csv
+	#check to see if this is the first split - if it is, don't add the header (it's already there)
+	if (( i > 0 ))
+	then
+	    #pass header back to parts
+	    #sed statement within quotes: 1i is its own command (see man page)
+	    sed -i "1i$header" $tempfile
+	fi
+        #rename file to .csv
 	mv $tempfile $tempfile.csv
+	(( i=i +1 ))
     done
 done
