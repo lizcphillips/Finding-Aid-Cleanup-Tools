e2413b72657143bd587005e50ef6c54ec77c764d
Liz Phillips <lizcphillips@users.noreply.github.com>
2021-02-22 17:17:43 -0800

    Added ability to do year-only & inclusive dates

--- Legacy finding aid cleanup tools/pass_normal_dates_to_attribute.py
+++ Legacy finding aid cleanup tools/pass_normal_dates_to_attribute.py
@@ -23,16 +23,21 @@ for filename in os.listdir(directory):
 
 # pass the date to the attribute            
 # https://stackoverflow.com/questions/63744550/beautifulsoup-xml-parsing-only-return-first-result            
-            i=1
+
             for soup.unitdate in soup.find_all("unitdate"):
-                date = soup.unitdate.string          
+                date = soup.unitdate.string       
+                #https://docs.python.org/3/library/re.html
                 month_year = re.compile(r"(([a-zA-Z]+) (\d{4}))")
-                month_day_year = re.compile(r"(([a-zA-Z]+) (\d+), (\d{4}))")           
- 
-                if 'normal' in soup.unitdate.attrs:
-                    continue
+                month_day_year = re.compile(r"(([a-zA-Z]+) (\d+), (\d{4}))")
+                multi_year = re.compile(r"(\d{4})-(\d{4})")
+                single_year = re.compile(r"(\d{4})")
+                if date == "Undated":
+                    date = "undated"
                 if date == "undated":
                     soup.unitdate.attrs['normal'] = "0000"
+                if 'normal' in soup.unitdate.attrs:
+                    continue
+                #https://stackabuse.com/converting-strings-to-datetime-in-python/
                 for date2 in re.findall(month_year, date):
                     date2 = datetime.datetime.strptime(date, "%B %Y")
                     date3 = datetime.date.strftime(date2, "%Y-%m")
@@ -41,6 +46,16 @@ for filename in os.listdir(directory):
                     date2 = datetime.datetime.strptime(date, "%B %d, %Y")
                     date3 = datetime.date.strftime(date2, "%Y-%m-%d")
                     soup.unitdate.attrs['normal'] = date3
+                for date2 in re.findall(single_year, date):
+                    soup.unitdate.attrs['normal'] = date2
+
+#multi-year needs to be handled as a tuple
+#https://codedestine.com/python-tuple-string-convert/
+
+                for date2 in re.findall(multi_year, date):
+                    separator = "/"
+                    soup.unitdate.attrs['normal'] = separator.join(date2)
+
 
 #print(soup.prettify)
 outputpath = path.join(outputdirectory, filename)
