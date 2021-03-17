47a3e53bbcf4db2067938ea6a25ab7ade4cffaac
Liz Phillips <lizcphillips@users.noreply.github.com>
2021-03-17 14:19:02 -0700

    Escaped special characters

--- MARC tools/ead_to_marc.bash
+++ MARC tools/ead_to_marc.bash
@@ -1,11 +1,11 @@
-#!/bin/bash
+ #!/bin/bash
 #pre-MARCEdit XSLT cleanups for finding aids
 path=/mnt/c/Users/ecphillips/ead_for_marc
 for file in $path/*.xml
 do
     tidy -emq -config /usr/share/tidy1.cfg $file
     cp $file $file.bak
-    cat $file | sed 's|xmlns="urn:isbn:1-931666-22-9"||g' | sed 's/xlink://g' | sed 's/linear feet//g' | sed 's|<head>Preferred Citation</head>||g' | sed 's|<head>History</head>||g' | sed 's|<head>Biography</head>||g'  | sed 's|<head>Scope and Contents</head>||g' | sed 's|.</p>|. </p>|g' | sed 's|<head>Biographical / Historical</head>||g' > $file.2
+    cat $file | sed 's|xmlns="urn:isbn:1-931666-22-9"||g' | sed 's/xlink://g' | sed 's/linear feet//g' | sed 's|<head>Preferred Citation</head>||g' | sed 's|<head>History</head>||g' | sed 's|<head>Biography</head>||g'  | sed 's|<head>Scope and Contents</head>||g' | sed 's|\.</p>|\. </p>|g' | sed 's|<head>Biographical / Historical</head>||g' | sed 's|\[Identification of item\], ||g' > $file.2
     mv $file.2 $file
 done
 diff -b  $path/*.bak $path/*.xml | less
